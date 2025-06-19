ec3d1358d8c54a3202aff099055e7ac4c71169ad
naiduyamini-12 <naiduyamini34@gmail.com>
2025-06-19 22:10:36 +0530

    Create Comments.java

---
+++ basic/Comments.java
@@ -0,0 +1,26 @@
+/**
+ * This is a simple Java program to demonstrate:
+ * 1. Single-line comment
+ * 2. Multi-line comment
+ * 3. Documentation comment
+ */
+
+public class CommentExample {
+
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello, this is a single-line comment example!");
+
+        /*
+         This is a multi-line comment.
+         It can span across multiple lines.
+        */
+        System.out.println("This is a multi-line comment example.");
+
+        /**
+         * This is a documentation comment.
+         * It is usually used to describe classes, methods, or fields.
+         */
+        System.out.println("This is a documentation comment example.");
+    }
+}
