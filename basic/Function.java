ff77b675102ed7a5f2136fcb50e7c154725517aa
naiduyamini-12 <naiduyamini34@gmail.com>
2025-06-20 08:39:13 +0530

    Create Function.java

---
+++ basic/Function.java
@@ -0,0 +1,13 @@
+class Function {
+
+    
+    public static void printName() {
+        System.out.println("My name is Yamini");
+    }
+
+    
+    public static void main(String[] args) {
+        
+        printName();
+    }
+}
