eea880da98d4546d74943a2274df8ff70fb38843
naiduyamini-12 <naiduyamini34@gmail.com>
2025-06-20 12:31:54 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+ class Student
+{
+
+String name;
+int age;
+void greet(String msg)
+{
+System.out.println(msg+","+name);
+}
+}
+public class Main
+{
+public static void main(String[] args)
+{
+Student s=new Student();
+s.name="Vidya";
+s.age=21;
+s.greet("Hello");
+}
+}
