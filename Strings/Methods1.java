c636ffa945f14308920185c34fddf0604ea76050
naiduyamini-12 <naiduyamini34@gmail.com>
2025-06-21 16:20:30 +0530

    Add files via upload

---
+++ Strings/Methods1.java
@@ -0,0 +1,147 @@
+/*
+class Methods1
+{
+public static void main(String[] args)
+{
+String s1="vidya Lakshmi";
+String s2="Vidya Lakshmi";
+System.out.println(s1+s2);
+System.out.println(s1.concat(s2));
+System.out.println(s1.toUpperCase());
+System.out.println(s2.toLowerCase());
+System.out.println(s1.compareTo(s2));
+System.out.println(s1.charAt(1));
+System.out.println(s1.length());
+System.out.println(s1.contains("Lakshmi"));
+System.out.println(s1.startsWith("vidya"));
+System.out.println(s1.endsWith("Lakshmi"));
+System.out.println(s2.indexOf("V"));
+System.out.println(s1.trim());
+System.out.println(s1.substring(0,2));
+System.out.println(s1.lastIndexOf("m"));
+}
+}
+vidya LakshmiVidya Lakshmi
+vidya LakshmiVidya Lakshmi
+VIDYA LAKSHMI
+vidya lakshmi
+32
+i
+13
+true
+true
+true
+0
+vidya Lakshmi
+vi
+11
+
+
+
+
+*/
+
+/*
+
+class Method1
+{
+public static void main(String[] args)
+{
+String s1="Hello";
+String s2=new String("World");
+char[] ch={'v','i','d','y','a'};
+String s3=new String(ch);
+byte[] b={65,66,67};
+String s4=new String(b);
+StringBuilder sn=new StringBuilder();
+s1.append("hi");
+s1.append("devi");
+String s5=sb.toString();
+String s6=String.valueOf(123);
+String s7="Good".concat("Night");
+String s8="Hello"+"Again";
+System.out.println(s1);
+System.out.println(s2);
+
+System.out.println(s3);
+
+System.out.println(s4);
+
+System.out.println(s5);
+
+System.out.println(s6);
+
+System.out.println(s7);
+
+System.out.println(s8);
+}
+}
+*/
+
+
+
+/*concate2
+
+class Methods
+{
+public static void main(String[]args)
+{
+String s1="sviet";
+String s2="sviet";
+String s3=new String("sviet");
+System.out.println(s1==s2);
+System.out.println(s1.equals(s3));
+s1=s1+s2;
+System.out.println(s1==s2); 
+String s6="svietsviet";
+System.out.println(s1==s6);
+}
+}
+*/
+
+/*lenght3
+class Methods1
+{
+public static void main(String[] args)
+{
+String s1="vidya Lakshmi";
+String s2="Vidya Lakshmi";
+System.out.println(s1.length());
+
+}
+}
+*/
+/*substring4
+class Methods1
+{
+public static void main(String[] args)
+{
+String s1="vidya Lakshmi";
+String s2="Vidya Lakshmi";
+System.out.println(s1.substring(0,2));
+System.out.println(s2.substring(5));
+}
+}
+*/
+class Methods1
+{
+public static void main(String[] args)
+{
+String s1="vidya Lakshmi";
+String s2="Vidya Lakshmi";
+System.out.println(s2.indexOf("V"));
+System.out.println(s2.indexOf("Yamini"));
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
